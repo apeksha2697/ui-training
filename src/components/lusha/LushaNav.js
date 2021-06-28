ee24c5ef6a9f6b0df4517a7df5b52ec7a0fdb2f2
apeksha2697 <apekshasingh664@gmail.com>
2021-06-28 09:21:19 +0530

    lusha page

--- src/components/lusha/LushaNav.js
+++ src/components/lusha/LushaNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Navbar,Nav} from 'react-bootstrap';
+import {Navbar,Nav, Container} from 'react-bootstrap';
 import logo from './images/logo.svg';
 import './LushaNav.scss';
 
